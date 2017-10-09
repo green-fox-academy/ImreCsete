7cb7a09b42dc79a5c87b2b4abe934b21ebf50fa6
ImreCsete <csete_imre@hotmail.com>
2017-10-09 15:08:01 +0200

    uploaded task

---
+++ week-02/day-1/printer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "printer.h"
+
+// Create a function called `printer`
+// which prints the input parameters
+// can have multiple number of arguments
+// store your variables in a .h file
+
+
+int main()
+{
+    printf("%s\n", PRINT);
+
+    return 0;
+}
