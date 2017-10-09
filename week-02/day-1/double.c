102cef7100f5c0b17d2d86c3a2ddb807b7995300
ImreCsete <csete_imre@hotmail.com>
2017-10-09 14:49:43 +0200

    upload task

---
+++ week-02/day-1/double.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "double.h"
+
+// create a function called `doubling` that doubles it's input parameter
+// again the parameters value should be stored in a .h file
+
+int doubling(int double_value);
+
+int main()
+{
+    printf("%d\n", doubling(0));
+
+    return 0;
+}
+
+int doubling(int double_value){
+    int doubles = input * 2;
+
+    return doubles;
+}
