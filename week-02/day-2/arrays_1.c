b4d0bbb4c82bacfbfe9c0591589bd365f7480a18
ImreCsete <csete_imre@hotmail.com>
2017-10-10 10:38:25 +0200

    added task

---
+++ week-02/day-2/arrays_1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    char my_name_array[] = "My Name";
+
+	//TODO:
+    // Print out the array character-by-character
+
+    for (int i = 0; i <= 6; i++){
+        printf("%c\n", my_name_array[i]);
+        }
+    return 0;
+}
