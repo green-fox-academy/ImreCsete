b6374f2c8288facc012b05cbabc880949e0ccff9
ImreCsete <csete_imre@hotmail.com>
2017-10-10 11:10:45 +0200

    shortened code

--- week-02/day-2/arrays_1.c
+++ week-02/day-2/arrays_1.c
@@ -2,12 +2,11 @@
 
 int main() {
     char my_name_array[] = "My Name";
-    int a = sizeof my_name_array;
 
 	//TODO:
     // Print out the array character-by-character
 
-    for (int i = 0; i < a; i++){
+    for (int i = 0; i < sizeof(my_name_array); i++){
         printf("%c\n", my_name_array[i]);
         }
     return 0;
