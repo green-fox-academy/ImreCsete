e98535078f203da3cb64fae2b4563f1d2e609d65
ImreCsete <csete_imre@hotmail.com>
2017-10-12 17:17:22 +0200

    discarded sorting algorithm

--- week-02/day-4/anagram.c
+++ week-02/day-4/anagram.c
@@ -19,34 +19,19 @@ int main()
         printf("The two words are not anagrams.\n");
     }
 
-    for (int a = 0; a < strlen(anagram_a); a++){ // dogdogdog
-        for (int b = a + 1; b < strlen(anagram_a); b++) { //dog
-            if (anagram_a[a] > anagram_a[b]){
-            int sort = anagram_a[a]; // god
-            anagram_a[a] = anagram_a[b]; //dgg
-            anagram_a[b] = sort; //dgo
-         }
-      }
-   }
-
-   int anagram = strcmp(anagram_a, anagram_b);
-
-   printf("%s\n%s", anagram_a, anagram_b);
-   printf("\n%d", anagram);
-
-   printf("%s", anagram_a);
-
     return 0;
 }
 
 char is_anagram(anagram_a, anagram_b)
 {
-   if (strlen(anagram_a) != strlen(anagram_b)){
+    if (strlen(anagram_a) != strlen(anagram_b)){
         return 0;
-    for (int a = 0; strlen(anagram_a); a++){
-
     }
-   } else
-        return 1;
 
+    int compare = (strcmp(anagram_a, anagram_b));
+
+    if (compare != 0){
+        return 0;
+    }
+    else return 1;
 }
