d1edd0b629967b7857f482f75720b62b9403e2ff
ImreCsete <csete_imre@hotmail.com>
2017-10-12 16:13:36 +0200

    added sorting algorithm

--- week-02/day-4/anagram.c
+++ week-02/day-4/anagram.c
@@ -5,7 +5,7 @@
 // Create a function named is anagram following your current language's style guide.
 // It should take two strings and return a boolean value depending on whether its an anagram or not.
 
-int is_anagram(anagram_a, anagram_b);
+char is_anagram(anagram_a, anagram_b);
 
 int main()
 {
@@ -18,13 +18,35 @@ int main()
     else {
         printf("The two words are not anagrams.\n");
     }
+
+    for (int a = 0; a < strlen(anagram_a); a++){ // dogdogdog
+        for (int b = a + 1; b < strlen(anagram_a); b++) { //dog
+            if (anagram_a[a] > anagram_a[b]){
+            int sort = anagram_a[a]; // god
+            anagram_a[a] = anagram_a[b]; //dgg
+            anagram_a[b] = sort; //dgo
+         }
+      }
+   }
+
+   int anagram = strcmp(anagram_a, anagram_b);
+
+   printf("%s\n%s", anagram_a, anagram_b);
+   printf("\n%d", anagram);
+
+   printf("%s", anagram_a);
+
     return 0;
 }
 
-int is_anagram(anagram_a, anagram_b)
+char is_anagram(anagram_a, anagram_b)
 {
    if (strlen(anagram_a) != strlen(anagram_b)){
         return 0;
+    for (int a = 0; strlen(anagram_a); a++){
+
+    }
    } else
         return 1;
+
 }
