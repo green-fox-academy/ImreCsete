ac9816adbf0cf941d4192b3fa499e4656fbf1ba2
ImreCsete <csete_imre@hotmail.com>
2017-10-13 13:11:31 +0200

    completed task

---
+++ week-02/day-4/palindrome_builder.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+//Create a function named create palindrome following your current language's style guide.
+//It should take a string, create a palindrome from it and then return it.
+
+void create_palindrome();
+
+int main()
+{
+    create_palindrome();
+
+    return 0;
+}
+
+
+void create_palindrome()
+{
+    char characters[100];
+
+    printf("Please enter some characters that will be returned as a palindrome: ");
+    scanf("%s", characters);
+
+    printf("%s", characters);
+    printf("%s", strrev(characters));
+}
+
