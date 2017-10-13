f3acaee91b75a851f00b532f98d5917711564f70
ImreCsete <csete_imre@hotmail.com>
2017-10-13 12:13:19 +0200

    added bubble sort for both arrays

--- week-02/day-4/anagram.c
+++ week-02/day-4/anagram.c
@@ -5,12 +5,12 @@
 // Create a function named is anagram following your current language's style guide.
 // It should take two strings and return a boolean value depending on whether its an anagram or not.
 
-char is_anagram(anagram_a, anagram_b);
+char is_anagram(char anagram_a[], char anagram_b[]);
 
 int main()
 {
-    char anagram_a[] = "dog";
-    char anagram_b[] = "god";
+    char anagram_a[] = "motherinlaw";
+    char anagram_b[] = "womanhitler";
 
     if (is_anagram(anagram_a, anagram_b) == 1){
         printf("The two words are anagrams.\n");
@@ -22,13 +22,38 @@ int main()
     return 0;
 }
 
-char is_anagram(anagram_a, anagram_b)
+char is_anagram(char anagram_a[], char anagram_b[])
 {
+    int n = strlen(anagram_a); // number of elements in array
+    int i; // number of passes
+    int j; // element to be swapped
+    char temp[strlen(anagram_a)]; // swap array
+
     if (strlen(anagram_a) != strlen(anagram_b)){
         return 0;
     }
+    for (i = 0; i < n; i++){
+        for (j = 0; j < (n - i); j++){
+            if (anagram_a[j] > anagram_a[j+1]){
+                temp[strlen(anagram_a)] = anagram_a[j];
+                anagram_a[j] = anagram_a[j+1];
+                anagram_a[j+1] = temp;
+            }
+        }
+    }
+
+    for (i = 0; i < n; i++){
+        for (j = 0; j < (n - i); j++){
+            if (anagram_b[j] > anagram_b[j+1]){
+                temp[strlen(anagram_a)] = anagram_b[j];
+                anagram_b[j] = anagram_b[j+1];
+                anagram_b[j+1] = temp;
+            }
+        }
+    }
 
     int compare = (strcmp(anagram_a, anagram_b));
+
 
     if (compare != 0){
         return 0;
