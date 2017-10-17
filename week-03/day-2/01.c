c81cdfeeb5cccf08d5d78ec9c00a1be7e0edb3cf
ImreCsete <csete_imre@hotmail.com>
2017-10-17 10:48:55 +0200

    completed task 01

---
+++ week-03/day-2/01.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <conio.h>
+
+main()
+{
+	int  i = 2;
+	float f = 13.5, result;
+
+	//TODO: print out the f/i quotient
+
+	result = f / i;
+
+	printf("%f", result);
+
+	//QUESTION: do you need explicit conversion?
+	//ANSWER: only if you print %d
+
+
+	return 0;
+}
+
