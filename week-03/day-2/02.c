3327bc497cb95a16725bab8ebc2063d7a4fc6615
ImreCsete <csete_imre@hotmail.com>
2017-10-17 10:58:39 +0200

    completed task 02

---
+++ week-03/day-2/02.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	double x = 1.2;
+
+    //TODO: write a program which adds up x and 1, and print out it rounded.
+
+    printf("%f", floor(x + 1));
+
+    return 0;
+}
