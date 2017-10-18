a6277f2e3c7aee2a27703942cca186a3d865a834
ImreCsete <csete_imre@hotmail.com>
2017-10-18 10:10:31 +0200

    Created project folder with calculator usage inside.

---
+++ week-03/Calculator/Calculator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+void startup();
+
+int main()
+{
+    startup();
+
+    return 0;
+}
+
+void startup()
+{
+    printf("\tCLI Calculator\n====================================\n");
+    printf("usage: [number] [operation] [number]\nCommands:\n\n");
+    printf("+\tsummation\n-\tsubtraction\n*\tmultiplication\n/\tdivision\n%\tdivision with remainder\n");
+    printf("^\tsquaring\n<\tsquare root\nlog\tlogarithm\nbinto\tbinary to hex or decimal\nhexto\thexadecimal to bin or dec\ndecto\tdecimal to bin or hex\n");
+    printf("====================================\nexit\texiting from the program\nclear\tclear the screen\nhelp\tprint usage\n");
+    printf("====================================\nHit enter to start!\n====================================");
+}
