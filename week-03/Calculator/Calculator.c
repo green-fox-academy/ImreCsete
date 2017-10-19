cf4f4da4643f2d48f4b813d77832c203ae926d3c
ImreCsete <csete_imre@hotmail.com>
2017-10-19 10:08:18 +0200

    made startup() more readable

--- week-03/Calculator/Calculator.c
+++ week-03/Calculator/Calculator.c
@@ -73,12 +73,28 @@ int main()
 
 void startup()
 {
-    printf("\tCLI Calculator\n====================================\n");
-    printf("usage: [number] [operation] [number]\nCommands:\n\n");
-    printf("+\tsummation\n-\tsubtraction\n*\tmultiplication\n/\tdivision\n%\tdivision with remainder\n");
-    printf("^\tsquaring\n<\tsquare root\nlog\tlogarithm\nbinto\tbinary to hex or decimal\nhexto\thexadecimal to bin or dec\ndecto\tdecimal to bin or hex\n");
-    printf("====================================\nexit\texiting from the program\nclear\tclear the screen\nhelp\tprint usage\n");
-    printf("====================================\nHit enter to start!\n====================================");
+    printf("\tCLI Calculator\n");
+    printf("====================================\n");
+    printf("usage: [number] [operation] [number]\n");
+    printf("Commands:\n\n");
+    printf("+\tsummation\n");
+    printf("-\tsubtraction\n");
+    printf("*\tmultiplication\n");
+    printf("/\tdivision\n");
+    printf("%\tdivision with remainder\n");
+    printf("^\tsquaring\n");
+    printf("<\tsquare root\n");
+    printf("log\tlogarithm\n");
+    printf("binto\tbinary to hex or decimal\n");
+    printf("hexto\thexadecimal to bin or dec\n");
+    printf("decto\tdecimal to bin or hex\n");
+    printf("====================================\n");
+    printf("exit\texiting from the program\n");
+    printf("clear\tclear the screen\n");
+    printf("help\tprint usage\n");
+    printf("====================================\n");
+    printf("Hit enter to start!\n");
+    printf("====================================");
 
     while (getchar() != '\n');
 
