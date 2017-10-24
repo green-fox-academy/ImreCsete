2228767aa52bd31962484d50691625c15855dc83
ImreCsete <csete_imre@hotmail.com>
2017-10-24 12:22:42 +0200

    completed task

--- week-04/day-1/02.c
+++ week-04/day-1/02.c
@@ -3,7 +3,7 @@
 int main()
 {
     int number = 1234;
-    int* number_pointer = &number;
+    int *number_pointer = &number;
 
     //TODO:
     // print the value of number using the "number_pointer"
