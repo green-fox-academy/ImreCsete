bcb8b41b0693bb54f8e1c8ff5fd8bbd12467a543
ImreCsete <csete_imre@hotmail.com>
2017-10-24 17:20:29 +0200

    completed task

---
+++ week-04/day-1/11.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+void calc(int a, int b, int *p_sum, int *p_mul);
+
+int main()
+{
+	int x, y;
+	x = 5;
+	y = 8;
+
+	int sum, mul;
+
+	//TODO: write a void function which calculates the sum and multiplication of x and y according to the function prototype.
+	//Use 'output parameters'. Print out these values.
+
+	calc(x, y, &sum, &mul);
+
+	printf("%d + %d = %d\n", x, y, sum);
+	printf("%d * %d = %d\n", x, y, mul);
+
+	return 0;
+}
+
+void calc(int a, int b, int *p_sum, int *p_mul)
+{
+    *p_sum = a + b;
+    *p_mul = a * b;
+}
