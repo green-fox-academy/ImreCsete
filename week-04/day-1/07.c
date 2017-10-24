06b151977aa269f9408b2cf6a902f03b3acc6c50
ImreCsete <csete_imre@hotmail.com>
2017-10-24 13:04:35 +0200

    fixed typing

--- week-04/day-1/07.c
+++ week-04/day-1/07.c
@@ -5,18 +5,18 @@ int main()
     int high_number = 6655;
     int low_number = 2;
 
-    int *hight_number_pointer = &low_number;
+    int *high_number_pointer = &low_number;
     int *low_number_pointer = &high_number;
 
     //TODO:
     // Please fix the problem and swap where the pointers point,
     // without using the "&" operator.
 
-    int temp = hight_number_pointer;
-    hight_number_pointer = low_number_pointer;
+    int temp = high_number_pointer;
+    high_number_pointer = low_number_pointer;
     low_number_pointer = temp;
 
-    printf("High is %d, Low is %d\n", *hight_number_pointer, *low_number_pointer);
+    printf("High is %d, Low is %d\n", *high_number_pointer, *low_number_pointer);
 
     return 0;
 }
