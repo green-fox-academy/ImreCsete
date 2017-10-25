557e686e29ed225048efd8bd70f1035af5485c6a
ImreCsete <csete_imre@hotmail.com>
2017-10-25 12:02:07 +0200

    completed task 01

---
+++ week-04/day-2/01.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdint.h>
+
+struct Computer {
+    float cpu_speed_GHz;
+    int ram_size_GB;
+    int bits;
+};
+
+int main()
+{
+    struct Computer computer = {3.2, 8, 32};
+
+    //TODO: Change the bits to 64
+
+    computer.bits = computer.bits * 2;
+
+    //TODO: print out the structure members
+
+    printf("%.1f, %d, %d", computer.cpu_speed_GHz, computer.ram_size_GB, computer.bits);
+
+    return 0;
+}
