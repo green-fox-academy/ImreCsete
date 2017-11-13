af8cc63ad59d4874420a061dd5503d9ab95c4831
ImreCsete <csete_imre@hotmail.com>
2017-11-13 10:16:07 +0100

    Completed task 00.cpp

---
+++ week-06/day-1/00.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Modify this program to greet you instead of the World!
+
+int main()
+{
+    string name = "";
+
+    cout << "Please enter your name: ";
+
+    cin >> name;
+
+    cout << endl << "Hello " << name << "!" <<endl;
+
+    return 0;
+}
