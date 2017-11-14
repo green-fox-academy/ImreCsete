8df67fae59cfcb1dfc4b5b7d88d0885fb0973698
ImreCsete <csete_imre@hotmail.com>
2017-11-14 12:09:31 +0100

    Completed task oop_04.cpp

---
+++ week-06/day-2/oop_04.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class House {
+    private:
+        string address;
+        unsigned int area;
+        unsigned int price;
+
+    public:
+        unsigned int get_price() {
+            return this->price * this->area;
+        }
+
+    House(string address, unsigned int area) {
+        this->address = address;
+        this->area = area;
+        this->price = 400;
+    }
+
+};
+
+    // The market price of the houses is 400 EUR / square meters
+    // Make the get_price() function work
+    // Create a constructor for the House class that takes it's address and area.
+
+int main()
+{
+
+    House house = House("Andrassy 66", 349);
+
+    cout << house.get_price();
+
+    return 0;
+}
