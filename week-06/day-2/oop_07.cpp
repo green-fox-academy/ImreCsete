20dc82f75143c3ba411846bf3ea6d0174ac86a16
ImreCsete <csete_imre@hotmail.com>
2017-11-14 16:42:25 +0100

    Added user input and made the code look better.

--- week-06/day-2/oop_07.cpp
+++ week-06/day-2/oop_07.cpp
@@ -42,37 +42,19 @@ class Pirate {
 
 int main()
 {
+    int drinks;
+    cout << "You are tending to your bar when a pirate walks in." << endl << "He asks for as much rum as you have in stock." << endl << "How many drinks do you want the pirate to drink? ";
+    cin >> drinks;
+
     Pirate pirate;
 
-    cout << pirate.get_rum() << " " << pirate.hows_goin_mate() << endl;
+    cout << endl << "You ask the pirate after each drink how is it going." << endl << endl;
 
-    pirate.drink_rum();
+    for (int i = 0; i < drinks; i++) {
+            pirate.drink_rum();
+        cout << "After " << pirate.get_rum() << " rum the pirate says " << pirate.hows_goin_mate() << endl;
 
-    cout << pirate.get_rum() << " " << pirate.hows_goin_mate() << endl;
-
-    pirate.drink_rum();
-
-    cout << pirate.get_rum() << " " << pirate.hows_goin_mate() << endl;
-
-    pirate.drink_rum();
-
-    cout << pirate.get_rum() << " " << pirate.hows_goin_mate() << endl;
-
-    pirate.drink_rum();
-
-    cout << pirate.get_rum() << " " << pirate.hows_goin_mate() << endl;
-
-    pirate.drink_rum();
-
-    cout << pirate.get_rum() << " " << pirate.hows_goin_mate() << endl;
-
-    pirate.drink_rum();
-
-    cout << pirate.get_rum() << " " << pirate.hows_goin_mate() << endl;
-
-    pirate.drink_rum();
-
-    cout << pirate.get_rum() << " " << pirate.hows_goin_mate() << endl;
+    }
 
     return 0;
 }
