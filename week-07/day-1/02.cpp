c0509f4c0d0d60bd8625b9bff32dcea4829deaeb
ImreCsete <csete_imre@hotmail.com>
2017-11-20 12:24:04 +0100

    Completed task 02.cpp with character in throw

---
+++ week-07/day-1/02.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+// Write a try - catch block.
+// Throw a character in the try block
+// Catch it in the catch block and write it out.
+
+int main() {
+
+    int a;
+    int b;
+
+    try {
+        cout << "This program will calculate the number 'c' by dividing 'a' with 'b'." << endl << endl << "Please don't divide by zero." << endl << endl;
+
+        cout << "Enter number a: ";
+        cin >> a;
+        cout << "Now enter number b: ";
+        cin >> b;
+
+        float c = (float)a / b;
+
+        if (b == 0) {
+            throw 'z';
+        } else cout << fixed << setprecision(1) << endl << a << " / " << b << " = " << c << endl << endl << "Congratulations, your ability to follow instructions is amazing!" << endl;
+
+    }   catch (char z) {
+        cout << "ERROR " << z << ": b is zero." << endl << "I am disappointed in you." << endl;
+    }
+
+	return 0;
+}
