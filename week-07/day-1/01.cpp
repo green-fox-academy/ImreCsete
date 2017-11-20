157c7996a9b9bc81745dbe2eaafd43d8b6a53887
ImreCsete <csete_imre@hotmail.com>
2017-11-20 12:13:46 +0100

    Completed task 01.cpp with divide by zero

--- week-07/day-1/01.cpp
+++ week-07/day-1/01.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -8,18 +9,25 @@ using namespace std;
 
 int main() {
 
-    int try_int = 0;
+    int a;
+    int b;
 
     try {
-        cout << "Please enter the number 42." << endl;
-        cin >> try_int;
+        cout << "This program will calculate the number 'c' by dividing 'a' with 'b'." << endl << endl << "Please don't divide by zero." << endl << endl;
 
-        if (try_int != 42) {
+        cout << "Enter number a: ";
+        cin >> a;
+        cout << "Now enter number b: ";
+        cin >> b;
+
+        float c = (float)a / b;
+
+        if (b == 0) {
             throw 0;
-        } else cout << "Congratulations, your ability to follow instructions is amazing!" << endl;
+        } else cout << fixed << setprecision(1) << endl << a << " / " << b << " = " << c << endl << endl << "Congratulations, your ability to follow instructions is amazing!" << endl;
 
     }   catch (int x) {
-        cout << "ERROR: " << try_int << " is not 42." << endl;
+        cout << "ERROR: b is 0." << endl << "I am disappointed in you." << endl;
     }
 
 	return 0;
