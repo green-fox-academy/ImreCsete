1ddef5c0f72312b82ae844913c05e6f4116bd1a3
ImreCsete <csete_imre@hotmail.com>
2017-11-20 17:31:53 +0100

    Completed task 12.cpp

---
+++ week-07/day-1/12.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Write a function that can throw two kinds of exceptions: int and const char*
+// It should receive an integer.
+// It should return a string from the char* array defined below.
+// If the integer is larger than 5, throw an integer exception. The value of the exception should be how much larger it is.
+// If the integer is less than 0, it should throw a const char* exception stating, that it is a negative number.
+//
+// Illustrate both cases in the main function.
+// HINT: Put each protected code segment in a new try-catch block.
+
+const char* sentence[6] = {"What", "a", "pleasant", "surprise", "this", "is."};
+
+const char* write_element(int element);
+
+int main()
+{
+    try {
+        int word;
+
+        cout << "Please enter the number of an element from the array: ";
+        cin >> word;
+
+        cout << write_element(word) << endl;
+
+        } catch (unsigned int x) {
+            cout << "Error: Number out of range by " << x << "." << endl;
+        } catch (const char *error) {
+            cout << "Error: " << error << "." << endl;
+        }
+
+    return 0;
+}
+
+const char* write_element(int element)
+{
+    if (sizeof(sentence) / sizeof(sentence[0]) <= element) {
+        throw (element - sizeof(sentence) / sizeof(sentence[0]));
+    }
+
+    else if (element < 0) {
+        throw "Negative number";
+    }
+
+    return sentence[element];
+
+}
