30e92e01051cb620cd9ad1a191c81a212d784120
ImreCsete <csete_imre@hotmail.com>
2017-11-23 11:00:12 +0100

    Added exit timeout in case program runs for a long time.

--- week-07/temperature_logger/main.cpp
+++ week-07/temperature_logger/main.cpp
@@ -1,13 +1,24 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <thread>
+#include <chrono>
+#include <cstdlib>
 
 #include "SerialPortWrapper.h"
 
 using namespace std;
 
+void command_list();
+int timeout();
+
 int main()
 {
+    vector <string> output;
+    vector <string> user_input;
+
+    command_list();
+
     vector<string> ports = SerialPortWrapper::listAvailablePorts();
     cout << "Number of found serial ports: " << ports.size() << endl;
     for (unsigned int i = 0; i < ports.size(); i++) {
@@ -19,6 +30,7 @@ int main()
         SerialPortWrapper *serial = new SerialPortWrapper("COM5", 115200);
         serial->openPort();
         string line;
+        thread t1(timeout);
         while(1){
         serial->readLineFromPort(&line);
         if (line.length() > 0){
@@ -26,5 +38,30 @@ int main()
         }
         }
         serial->closePort();
+
     return 0;
+}
+
+void command_list()
+{
+    cout << "Temperature Logger Application" << endl
+        <<  "==============================" << endl
+        << "Commands:" << endl
+        << "h        Show command list" << endl
+        << "o        Open port" << endl
+        << "s        Start logging / Stop logging" << endl
+        << "c        Close port" << endl
+        << "l        List after error handling" << endl
+        << "e        Exit from the program" << endl << endl;
+}
+
+int timeout()
+{
+    using namespace chrono;
+
+    auto start = high_resolution_clock::now();
+
+    this_thread::sleep_for(minutes(10));
+
+    exit(0);
 }
