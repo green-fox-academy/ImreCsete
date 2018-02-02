8619c07f6ef670d30056511390d0b7fcc16c197d
ImreCsete <csete_imre@hotmail.com>
2018-02-02 09:37:12 +0100

    Added exam project

---
+++ final_exam/excercise_number_10/final_exam/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//TODO: Create a function that takes an array of integers and returns the average of the even numbers from that array
+
+int main()
+{
+
+}
