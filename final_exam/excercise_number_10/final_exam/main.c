3c59ad29c3f0090e68356ec38cd08dbc4f5c426d
ImreCsete <csete_imre@hotmail.com>
2018-02-02 09:44:15 +0100

    Created array and filled it with seeded random number generator.

--- final_exam/excercise_number_10/final_exam/main.c
+++ final_exam/excercise_number_10/final_exam/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 //TODO: Create a function that takes an array of integers and returns the average of the even numbers from that array
 
@@ -7,7 +8,20 @@ float arr_even_avg(int array);
 
 int main()
 {
+    srand((int)time(NULL));
+    int arr[10];
 
+    for (int i = 0; i < (sizeof(arr) / sizeof(arr[0])); i++) {
+        arr[i] = rand() % 500 + 1;
+    }
+
+    printf("The elements of the array are: ");
+
+    for (int i = 0; i < (sizeof(arr) / sizeof(arr[0])); i++) {
+        printf("%d, ", arr[i]);
+    }
+
+    printf("\n");
 }
 
 float arr_even_avg(int array)
