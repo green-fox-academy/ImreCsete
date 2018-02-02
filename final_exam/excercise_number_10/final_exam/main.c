dbf543a024f0d923f2297ee050e725cf6ec56028
ImreCsete <csete_imre@hotmail.com>
2018-02-02 09:38:35 +0100

    Added function

--- final_exam/excercise_number_10/final_exam/main.c
+++ final_exam/excercise_number_10/final_exam/main.c
@@ -3,7 +3,14 @@
 
 //TODO: Create a function that takes an array of integers and returns the average of the even numbers from that array
 
+float arr_even_avg(int array);
+
 int main()
+{
+
+}
+
+float arr_even_avg(int array)
 {
 
 }
