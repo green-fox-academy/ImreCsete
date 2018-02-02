4098fabf07a2ab42035e1a6f5632f8b912c66ed8
ImreCsete <csete_imre@hotmail.com>
2018-02-02 10:12:38 +0100

    Fixed passing into function, starting sorting out array size

--- final_exam/excercise_number_10/final_exam/main.c
+++ final_exam/excercise_number_10/final_exam/main.c
@@ -4,7 +4,7 @@
 
 //TODO: Create a function that takes an array of integers and returns the average of the even numbers from that array
 
-float arr_even_avg(int *array);
+float arr_even_avg(int array[], int size);
 
 int main()
 {
@@ -21,21 +21,30 @@ int main()
         printf("%d, ", arr[i]);
     }
 
-    printf("\nThe avg of even numbers are: %f\n", arr_even_avg(&arr));
+    printf("\n\n");
+
+    arr_even_avg(arr, 10);
+
+    //printf("\nThe avg of even numbers are: %f\n", arr_even_avg(&arr));
 }
 
-float arr_even_avg(int *array)
+float arr_even_avg(int array[], int size)
 {
-    int even_arr[10];
+    int even_arr[size];
+    int j = 0;
     int sum = 0;
     float avg = 0;
 
-    for (int i = 0; i < (sizeof(array)/sizeof(array[0])); i++) {
+    for (int i = 0; i < size; i++) {
         if (array[i] % 2 == 0) {
-            even_arr[i] = array[i];
-            sum += even_arr[i];
+            even_arr[j] = array[i];
+            sum += even_arr[j];
+            j++;
         }
     }
 
-    return avg = (sum / (sizeof(even_arr)/sizeof(even_arr[0])));
+    for (int i = 0; i < size; i++) {
+        printf("%d, ", even_arr[i]);
+    }
+    //return avg = (sum / (sizeof(even_arr)/sizeof(even_arr[0])));
 }
